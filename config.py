32dbea00e9ce8df1fe90d7a5d10a0cccd0d0413e
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-04-27 09:02:22 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
+TELEGRAM_CHANNEL_ID = int(os.getenv("TELEGRAM_CHANNEL_ID"))  # Ensure integer conversion
 DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
