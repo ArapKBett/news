047e61bed2ab2b65d4014601c9a1ebabfd46cf72
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-04-28 23:31:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-NEWS_API_KEY = os.getenv("NEWS_API_KEY")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-TELEGRAM_CHANNEL_ID = int(os.getenv("TELEGRAM_CHANNEL_ID"))  # Ensure integer conversion
-DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
+CONFIG = {
+    "newsapi": {
+        "key": os.getenv("NEWS_API_KEY"),
+        "url": "https://newsapi.org/v2/"
+    },
+    "gnews": {
+        "key": os.getenv("GNEWS_API_KEY"),
+        "url": "https://gnews.io/api/v4/"
+    },
+    "guardian": {
+        "key": os.getenv("GUARDIAN_API_KEY"),
+        "url": "https://content.guardianapis.com/"
+    },
+    "telegram": {
+        "token": os.getenv("TELEGRAM_BOT_TOKEN"),
+        "channel": int(os.getenv("TELEGRAM_CHANNEL_ID"))
+    },
+    "discord": {
+        "token": os.getenv("DISCORD_BOT_TOKEN"),
+        "channel": int(os.getenv("DISCORD_CHANNEL_ID"))
+    }
+}
